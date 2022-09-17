ca66d8b4218b7e7ad98f9dff27617243b7ac4251
nageshanna <104970894+nageshanna@users.noreply.github.com>
2022-09-17 18:04:56 +0530

    variable_output

---
+++ main.tf
@@ -0,0 +1,15 @@
+output "string_output" {
+  value = var.string_example
+}
+output "number_output" {
+  value = var.number_example
+}
+output "bool_output" {
+  value = var.bool_example
+}
+output "list_output" {
+  value = var.list_example
+}
+output "object_output" {
+  value = var.object_example
+}
---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+string_example = "Hi Nagnath"
+number_example = 1234
+bool_example = true
+list_example = (["Nagnath","Navnath","Ram"])
+object_example = {
+  name = "TagName"
+  new_tag = 789
+}
